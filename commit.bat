1b0b59bb9762a034656a3b5be3f2371488adb012
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-22 15:40:01 -0800

    Update commit.bat

--- commit.bat
+++ commit.bat
@@ -1,16 +1,32 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set repo_path=C:\Work\Codes\HMI\VS\WPF_Tutorials
-set file_names=App.xaml App.xaml.cs AssemblyInfo.cs MainWindow.xaml MainWindow.xaml.cs WPFTutorial.csproj
+set repo_path=C:\Work\Codes\HMI\VS\CSharp_Tutorials
+set file_names=Program.cs SharpTutorial.csproj
+set "episodeName=Ep2_SyntaxBasics"
+
+set "sln_commit=false"
 
 for %%F in (%file_names%) do (
     
-    set file_path=.\Ep24_RelayCommandMVVM\WPFTutorial\%%F
-    set commit_message=Create Ep24 %%F
+    set file_path=.\!episodeName!\SharpTutorial\%%F
+    set commit_message=Create !episodeName! %%F
 
     cd %repo_path%
     echo !file_path!
+    git add !file_path!
+
+    git commit -m "!commit_message!"
+    git push
+
+    echo Commit and Push Complete!
+)
+
+
+
+if "%sln_commit%"=="true" (
+    set file_path=.\!episodeName!\SharpTutorial.sln
+    set commit_message=Create !episodeName! SharpTutorial.sln
     git add !file_path!
 
     git commit -m "!commit_message!"
