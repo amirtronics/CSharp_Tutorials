2f6f68c340f505e2a40207ef208b8477a67cfcba
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-25 18:53:13 -0800

    Update Ep4 Program.cs

--- Ep4_VariablesDataTypes/SharpTutorial/Program.cs
+++ Ep4_VariablesDataTypes/SharpTutorial/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string customMessage;
+            customMessage = "Custom Message!";
+
+            int number = 565;
+            long bigNumber = 1231241241;
+
+            float myFloat = 5.01f;
+            double myDouble = 1.235155;
+            decimal myDecimal = 1.21515154;
+
+            bool isTutorialBoring = false;
+            bool isTutorialLong = true;
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
