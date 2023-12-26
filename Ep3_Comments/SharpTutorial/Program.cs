a64eb9c5ae2a3effe687af8dba65b46ee90913a3
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-25 18:04:06 -0800

    Update Ep3 Program.cs

--- Ep3_Comments/SharpTutorial/Program.cs
+++ Ep3_Comments/SharpTutorial/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 0;
+            var Num = 0;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
