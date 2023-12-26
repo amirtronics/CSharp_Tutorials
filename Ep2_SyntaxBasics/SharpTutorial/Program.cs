3190810c07e8f4ef2b3983b9bb128ed46ea77da5
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-25 17:58:14 -0800

    Update Ep2 Program.cs

--- Ep2_SyntaxBasics/SharpTutorial/Program.cs
+++ Ep2_SyntaxBasics/SharpTutorial/Program.cs
@@ -6,6 +6,8 @@ namespace SharpTutorial // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            var num = 0;
+            var Num = 0;
             Console.WriteLine("Hello World!");
         }
     }
