fd7e0d44bed66eea44f2dd2596fa47a58e5baec6
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-27 13:36:29 -0800

    Update Ep7_CodingChallenge Program.cs

--- Ep7_CodingChallenge/SharpTutorial/Program.cs
+++ Ep7_CodingChallenge/SharpTutorial/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+
+            Console.ReadLine();
+        }
+    }
+}
