edeb91d6bc1d3c92c633a898f634329ddec30e61
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-27 08:48:06 -0800

    Update Ep5 Program.cs

--- Ep5_Operators/SharpTutorial/Program.cs
+++ Ep5_Operators/SharpTutorial/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //math
+            // + - * /
+            int number1 = 3;
+            int number2 = 4;
+            int result = number1 + number2;
+
+            Console.WriteLine(result);
+
+
+            //assignment 
+
+
+            //relational 
+
+
+            //logical
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
