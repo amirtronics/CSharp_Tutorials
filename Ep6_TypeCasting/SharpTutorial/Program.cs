d89d7ca02058eb5472ff61708e9275d0aa836f04
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-27 10:32:17 -0800

    Update Ep6_TypeCasting Program.cs

--- Ep6_TypeCasting/SharpTutorial/Program.cs
+++ Ep6_TypeCasting/SharpTutorial/Program.cs
@@ -6,7 +6,17 @@ namespace SharpTutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number1 = 15;
+            double number2 = 8.5;
+
+            // implicit casting (C# can do it for you)
+            // the types have to be compatible for c# to take action
+            double number3 = number1;
+
+            //explicit casting (you have to tell it what to do)
+            int number4 = (int)number2; //lose our precision!!! 8.5 -> 8
+
+            Console.WriteLine(number4);
             Console.ReadLine();
         }
     }
