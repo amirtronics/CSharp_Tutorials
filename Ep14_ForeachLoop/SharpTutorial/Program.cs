c3d3bf890cc7874eb437f61a71c1da2527659c41
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-28 09:50:46 -0800

    Update Ep14_ForeachLoop Program.cs

--- Ep14_ForeachLoop/SharpTutorial/Program.cs
+++ Ep14_ForeachLoop/SharpTutorial/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myNames = new List<string> { "Paul", "Jimmy", "Wanda", "Beatrice"};
+            List<int> myInts = new List<int> { 1, 2, 3, 4 };
+
+
+            foreach (string name in myNames)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
