249d675ade4c794a3a84698501bfde9d0b1d0dae
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-27 16:49:42 -0800

    Update Ep8_IfStatement Program.cs

--- Ep8_IfStatement/SharpTutorial/Program.cs
+++ Ep8_IfStatement/SharpTutorial/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isActive = true;
+
+            if(isActive)
+            {
+                Console.WriteLine("The system is active");
+            }
+            else
+            {
+                Console.WriteLine("The system is not active!");
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+
+}
