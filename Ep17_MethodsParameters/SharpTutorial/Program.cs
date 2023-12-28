5eda85f825dd6fa0ffb9d9f75712cdf86432c07e
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-28 14:29:15 -0800

    Update Ep17_MethodsParameters Program.cs

--- Ep17_MethodsParameters/SharpTutorial/Program.cs
+++ Ep17_MethodsParameters/SharpTutorial/Program.cs
@@ -6,8 +6,21 @@ namespace SharpTutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            int userNumber = 1234;
+            int userNumber2 = 2345;
+            string userName = "Kampa";
+
+
+            PrintResult(userNumber, userNumber2, userName);
+        }
+
+        // parameter - type variablename
+        // optional parameters have to be at the end of your parameter list
+        // optional - type variableName = defaultValue
+
+        static void PrintResult(int number, int number2 = 20, string name = "User")
+        {
+            Console.WriteLine(1);
         }
     }
 }
