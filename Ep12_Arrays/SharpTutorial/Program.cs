7cb8ed6ed258b9c6352b7481ddbd8232f0fc850a
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-28 09:08:48 -0800

    Update Ep12_Arrays Program.cs

--- Ep12_Arrays/SharpTutorial/Program.cs
+++ Ep12_Arrays/SharpTutorial/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myInts = { 1, 2, 3, 4 };
+            int[] myInts2 = new int[10];
+            int[] myInts3;
+            myInts3 = new int[] { 1, 2, 3, 4 };
+
+            int myFirstInt = myInts[0];
+            int myFourthInt = myInts[3];
+
+            string[] myNames = { "Bill", "George", "Frank", "Nancy", "Pam" };
+
+            for (int i = 0; i < myInts.Length; i++)
+            {
+                Console.WriteLine(myNames[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
