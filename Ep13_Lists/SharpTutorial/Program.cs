95ac4a1462cf8299cb31e83fd179df4e11fe326f
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-28 09:22:50 -0800

    Update Ep13_Lists Program.cs

--- Ep13_Lists/SharpTutorial/Program.cs
+++ Ep13_Lists/SharpTutorial/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myNames;
+
+            myNames = new List<string> { "JIM", "STEVE", "MARSHA" };
+
+            string myFirstName = myNames[0];
+            string myThirdName = myNames[2];
+
+            for (int i = 0; i < myNames.Count; i++)
+            {
+                Console.WriteLine(myNames[i]);
+            }
+
+            myNames.Add("KEVIN");
+            myNames.Add("PAM");
+
+            myNames.RemoveAt(0);
+            myNames.Clear();
+
+            for (int i = 0; i < myNames.Count; i++)
+            {
+                Console.WriteLine(myNames[i]);
+            }
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
