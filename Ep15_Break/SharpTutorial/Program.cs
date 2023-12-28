c0c44b61998f45445c08ceae33a754fc8a271749
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-28 13:56:57 -0800

    Update Ep15_Break Program.cs

--- Ep15_Break/SharpTutorial/Program.cs
+++ Ep15_Break/SharpTutorial/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Adam", "Jamie", "Steve", "Kim", "Chelle", "Tom" };
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (names[i] == "Chelle")
+                {
+                    break;
+                }
+
+            }
+
+            while (true)
+            {
+                Console.Write("Give me your name: ");
+                string inpu = Console.ReadLine();
+
+                if (inpu == "exit")
+                    break;
+
+            }
+
+            Console.WriteLine("We're done");
+            Console.ReadLine();
+
+        }
+    }   
+}
