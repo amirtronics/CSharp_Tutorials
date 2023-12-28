6cf2a9cb823586bb1516e99b8a89dc2f0f878126
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-28 14:18:18 -0800

    Update Ep16_Methods Program.cs

--- Ep16_Methods/SharpTutorial/Program.cs
+++ Ep16_Methods/SharpTutorial/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHeader();
+
+            while (true)
+            {
+                string fullname = GetUsersFullName();
+
+                Console.WriteLine(fullname);
+                Console.WriteLine();
+            }
+
+        }
+
+        // static - modifer, don't worry about this yet, you just have to have it for now
+        // void - return type, what our method gives back, it can be any type but if you want to return nothing, use void
+        // method name, case sensitive, can be anything (not reserved)
+        // parameter list (arguments), don't worry about these yet
+        // method's code block
+
+        static void PrintHeader()
+        {
+            Console.WriteLine("Welcome to our program!");
+            Console.WriteLine("-----------------------------------");
+
+        }
+
+        static string GetUsersFullName()
+        {
+            Console.Write("Enter your fist name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            string fullName = firstName + " " + lastName;
+
+            return fullName;
+        }
+    }
+
+}
