c1cb0f50b77199a3367b3a8f592f2c208ddf7035
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-27 17:48:13 -0800

    Update Ep10_WhileLoop Program.cs

--- Ep10_WhileLoop/SharpTutorial/Program.cs
+++ Ep10_WhileLoop/SharpTutorial/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool hasValidInput = false;
+            bool isTheUserSure = false;
+
+            while(!hasValidInput)
+            {
+                Console.WriteLine("Are you sure? Enter yes or no: ");
+                string input = Console.ReadLine();
+
+                if(input == "yes" || input == "no")
+                {
+                    hasValidInput = true;
+                    if(input == "yes")
+                    {
+                        isTheUserSure = true;
+                    }
+                    else
+                    {
+                        isTheUserSure = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, try again");
+
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
