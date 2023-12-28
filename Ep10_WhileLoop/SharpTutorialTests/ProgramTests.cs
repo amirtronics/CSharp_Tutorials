044bd8323c0cb94cafce9d6df24649b0664aa514
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-27 18:01:43 -0800

    Add test unit

---
+++ Ep10_WhileLoop/SharpTutorialTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpTutorial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpTutorial.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void GetUserConfirmationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
