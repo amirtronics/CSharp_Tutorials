46e701d23d0922c19a12e81c6c97f082d4b43948
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 09:59:48 -0800

    Update Ep22_Constructors Person.cs

---
+++ Ep22_Constructors/SharpTutorial/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace SharpTutorial
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string first, string last)
+        {
+            firstName = first;
+            lastName = last;
+        }
+
+        public Person(string first, string last, int age)
+        {
+
+        }
+
+        public string GetFullName()
+        { 
+            return firstName + " " + lastName; 
+        }
+    }
+}
