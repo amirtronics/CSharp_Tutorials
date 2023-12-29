c83cb866cf7b125db124e3b59108ffc342b65ce6
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 11:03:14 -0800

    Update Ep23_Properties Person.cs

--- Ep23_Properties/SharpTutorial/Person.cs
+++ Ep23_Properties/SharpTutorial/Person.cs
@@ -8,9 +8,17 @@ namespace SharpTutorial
 {
     internal class Person
     {
+        public Person(string first, string last) 
+        {
+            FirstName = first;
+            LastName = last;
+        }
+
         //auto property
         public string FirstName { get; set; }
-
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        
 
         /* Property
         public string FirstName
