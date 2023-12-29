bd57966305b6badeb41f50fe23ddf7e450a8c900
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-28 17:18:50 -0800

    Create Ep20 Person.cs

---
+++ Ep20_OOP/SharpTutorial/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace SharpTutorial
+{
+    internal class Person
+    {
+        public string Firstname;
+        public string Lastname;
+        public int Age;
+        public string FavouriteColor;
+
+        public string GerFullName()
+        {
+            return Firstname + " " + Lastname;
+        }
+
+    }
+}
