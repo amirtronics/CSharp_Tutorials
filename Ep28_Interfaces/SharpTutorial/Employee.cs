3469a7a93864d997f076069a13777c6c44a22171
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 15:45:26 -0800

    Update Ep28_Interfaces Employee.cs

---
+++ Ep28_Interfaces/SharpTutorial/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpTutorial
+{
+    internal class Employee : IPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public int EmployerNumber { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public string GetDescription()
+        {
+            return GetFullName() + ", " + EmployerNumber;
+        }
+    }
+}
