f7be0f7856276f5c3589c4efbb405c2d98ff866f
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 11:13:35 -0800

    Create Ep24_InheritanceP1 Program.cs

--- Ep24_InheritanceP1/SharpTutorial/Program.cs
+++ Ep24_InheritanceP1/SharpTutorial/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Kampa";
+
+            Employee employee = new Employee();
+            employee.EmployeeNumber = 1;
+            employee.FirstName = "Kampa";
+            string name  = employee.GetFullName();
+
+            Doctor doc = new Doctor();
+            doc.FirstName = "";
+            doc.EmployeeNumber = 1;
+            doc.Specialty = "";
+
+            Console.ReadLine();
+        }
+    }
+}
