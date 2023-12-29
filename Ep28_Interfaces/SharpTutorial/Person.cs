d211fb1814d3f4e5d78dd3d65e8f9d42e260b0a4
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 15:46:01 -0800

    Update Ep28_Interfaces Person.cs

---
+++ Ep28_Interfaces/SharpTutorial/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace SharpTutorial
+{
+    internal interface IPerson
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string GetFullName();
+        string GetDescription();
+
+
+        /*
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract string GetDescription();
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+        */
+    }
+}
