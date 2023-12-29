b945c3394619784bbdebb7bbf33066112102ebc4
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 10:58:34 -0800

    Update Ep23_Properties Person.cs

---
+++ Ep23_Properties/SharpTutorial/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpTutorial
+{
+    internal class Person
+    {
+        //auto property
+        public string FirstName { get; set; }
+
+
+        /* Property
+        public string FirstName
+        {
+            get { return FirstName; }
+            set
+            {
+                FirstName = value;
+                //UpdateUI(value)
+            }
+        }
+
+        private string firstName;
+        */
+    }
+}
