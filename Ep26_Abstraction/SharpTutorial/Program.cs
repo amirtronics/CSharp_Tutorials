0712258271485fb98c8e980c3cda9fdbf11fbaf1
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 12:43:00 -0800

    Update Ep26_Abstraction Program.cs

--- Ep26_Abstraction/SharpTutorial/Program.cs
+++ Ep26_Abstraction/SharpTutorial/Program.cs
@@ -6,7 +6,24 @@ namespace SharpTutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee employee = new Employee();
+            employee.FirstName = "Kampa";
+            employee.LastName = "Plays";
+            employee.EmployerNumber = 1;
+
+
+            Person person = employee;
+            Console.WriteLine(person.GetDescription());
+
+            Doctor doctor = new Doctor();
+            doctor.FirstName = "Doc";
+            doctor.LastName = "Holiday";
+            doctor.EmployerNumber = 1;
+            doctor.Specialty = "Brain stuff";
+
+            Person person2 = doctor;
+            Console.WriteLine(person2.GetDescription());
+
             Console.ReadLine();
         }
     }
