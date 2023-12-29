21a84bb3a269413ffdd8f826964cc7ec25f77fcb
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 15:45:19 -0800

    Update Ep28_Interfaces Program.cs

--- Ep28_Interfaces/SharpTutorial/Program.cs
+++ Ep28_Interfaces/SharpTutorial/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Boat boat = new Boat();
+
+            List<IMove> movableEntities = new List<IMove>() { dog, boat};
+            foreach (var entity in movableEntities)
+                entity.Move();
+
+            List<IMakeSound> soundables = new List<IMakeSound>() { dog, boat };
+            foreach (var entity in soundables)
+                entity.MakeSound();
+
+            //foreach thing that CAN move
+            // thing.Move()
+
+            /*
+            Employee employee = new Employee
+            {
+                FirstName = "Kampa",
+                LastName = "Plays",
+                EmployerNumber = 99
+            };
+
+            IPerson myPerson = employee;
+
+            Console.WriteLine(myPerson.GetDescription());
+            */
+
+            Console.ReadLine();
+        }
+    }
+}
