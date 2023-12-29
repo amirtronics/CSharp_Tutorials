cc20ec62004d706f0839ef52af6657dc0ca5b12d
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 12:43:04 -0800

    Update Ep26_Abstraction Person.cs

---
+++ Ep26_Abstraction/SharpTutorial/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpTutorial
+{
+    //abstraction - OOP concept
+    // hide away certain logic, expose only what we need at any given level
+    // you can create an object of an abstract class
+    //base class (parent)
+
+    abstract internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract string GetDescription();
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
