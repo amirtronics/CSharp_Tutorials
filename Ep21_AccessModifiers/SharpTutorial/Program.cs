f75a2d53fc5410bdd7664e4da8cb2a90ae6f83c4
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-28 19:31:27 -0800

    Update Ep21_AccessModifiers Program.cs

--- Ep21_AccessModifiers/SharpTutorial/Program.cs
+++ Ep21_AccessModifiers/SharpTutorial/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        /*
+         * 
+         * Access Modifiers: Used to define where the item (class/method/field/etc) can be used 
+         * Enables ENCAPSULATION (OOP Concept)
+         *      - Keeping data together with its relevant methods
+         *      - Restricting direct access to this data to keep things simple and safe from unwanted modifications.
+         ------------------------------------------------------
+                public: Can be used in any class
+                private: Can only be used in the class it is in
+        -------------------------------------------------------
+                protected: Can only be used in the class it is in or classes derived from it.
+                internal: Can be used in any class in the current assembly
+                protected internal: Can be used in any class in the current assembly or classes in other assemblies derived from the class it is in 
+                private protected: Can only be used in the class it is in or classes derived fromit within the current assembly.
+        */
+
+
+
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Kampa";
+            person.LastName = "Plays";
+            person.Age = 20;
+
+            Console.WriteLine(person.GetFullName());
+            Console.ReadLine();
+        }
+    }
+}
