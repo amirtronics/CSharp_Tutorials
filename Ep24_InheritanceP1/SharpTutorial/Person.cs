86675625dd07acc3feb90b5fc381624711bb0562
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 11:13:43 -0800

    Update Ep24_InheritanceP1 Person.cs

---
+++ Ep24_InheritanceP1/SharpTutorial/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace SharpTutorial
+{
+    //base class (parent)
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetFullName() 
+        { 
+            return FirstName + " " + LastName; 
+        }
+    }
+}
