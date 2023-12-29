e9f6333dcec9bc7091b1395af3ea9e7507ca97ed
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 14:29:20 -0800

    Update Ep27_BaseKyeword Doctor.cs

---
+++ Ep27_BaseKyeword/SharpTutorial/Doctor.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpTutorial
+{
+    internal class Doctor : Employee
+    {
+        public Doctor(string first, string last, int number, string specialty) : base(first, last, number)
+        {
+            Specialty = specialty;
+        }
+
+        public string Specialty { get; private set; }
+
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() + ", " + Specialty;
+        }
+    }
+}
+
+
