eb27ef47e378050fe50245402a3545ebb886c516
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 14:29:18 -0800

    Update Ep27_BaseKyeword Program.cs

--- Ep27_BaseKyeword/SharpTutorial/Program.cs
+++ Ep27_BaseKyeword/SharpTutorial/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Kampa", "Plays", 1);
+
+            Person person = employee;
+            Console.WriteLine(person.GetDescription());
+
+            Doctor doctor = new Doctor("Doc", "Holiday", 1, "Brain stuff");
+
+            Person person2 = doctor;
+            Console.WriteLine(person2.GetDescription());
+
+            Console.ReadLine();
+        }
+    }
+}
