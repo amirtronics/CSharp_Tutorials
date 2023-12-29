77f36a9131d9472f2ff457043145f5f925d4277a
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 11:14:12 -0800

    Update Ep24_InheritanceP1 Doctor.cs

--- Ep24_InheritanceP1/SharpTutorial/Doctor.cs
+++ Ep24_InheritanceP1/SharpTutorial/Doctor.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SharpTutorial
+﻿namespace SharpTutorial
 {
-    internal class Doctor
+    internal class Doctor : Employee
     {
+        public string Specialty { get; set; }
     }
 }
