ce6d8bd3df77e5e72617383a0751550a2fac379a
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 12:19:05 -0800

    Create Ep25_Polymorphism Person.cs

---
+++ Ep25_Polymorphism/SharpTutorial/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpTutorial
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
