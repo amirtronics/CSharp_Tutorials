bd3e7f8a037d6458d4c3e57e04b409aad06fdc86
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 14:29:23 -0800

    Update Ep27_BaseKyeword Person.cs

---
+++ Ep27_BaseKyeword/SharpTutorial/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpTutorial
+{
+    abstract internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract string GetDescription();
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
