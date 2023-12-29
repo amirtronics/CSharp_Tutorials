7bacc77947dfc8bd0030e272092af35ff5524001
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 12:43:02 -0800

    Update Ep26_Abstraction Employee.cs

---
+++ Ep26_Abstraction/SharpTutorial/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace SharpTutorial
+{
+    internal class Employee : Person
+    {
+        public int EmployerNumber { get; set; }
+
+        public override string GetDescription()
+        {
+            return GetFullName() + ", " + EmployerNumber;
+        }
+    }
+}
