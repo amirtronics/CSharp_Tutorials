07a61d64a159b1fb1d94dfe4a8bc49006fc3520a
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 14:29:22 -0800

    Update Ep27_BaseKyeword Employee.cs

---
+++ Ep27_BaseKyeword/SharpTutorial/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace SharpTutorial
+{
+    internal class Employee : Person
+    {
+        public int EmployerNumber { get; set; }
+
+        public Employee(string first, string last, int number)
+        {
+            FirstName = first;
+            LastName = last;
+            EmployerNumber = number;
+        }
+
+        public override string GetDescription()
+        {
+            return GetFullName() + ", " + EmployerNumber;
+        }
+    }
+}
