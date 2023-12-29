38dac9fed2059e84186903a53ba076e12d066407
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-28 17:17:27 -0800

    Update Ep20_OOP Program.cs

--- Ep20_OOP/SharpTutorial/Program.cs
+++ Ep20_OOP/SharpTutorial/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+
+            while (true)
+            {
+                Console.Write("Enter your first name: ");
+                string firstNameInput = Console.ReadLine();
+
+                if (firstNameInput == "done")
+                {
+                    Console.Clear();
+                    break;
+                }
+
+                Console.Write("Enter your last name: ");
+                string lastNameInput = Console.ReadLine();
+                Console.Write("Enter your age: ");
+                int ageInput = Convert.ToInt32(Console.ReadLine());
+
+                Person person = new Person();
+                person.Firstname = firstNameInput;
+                person.Lastname = lastNameInput;
+                person.Age = ageInput;
+
+                people.Add(person);
+
+                Console.Clear();
+
+            }
+
+            foreach(Person person in people)
+            {
+                string fullname = person.GerFullName();
+            }
+
+        }
+    }
+}
