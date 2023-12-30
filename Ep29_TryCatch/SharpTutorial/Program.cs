5f0f706444f8abc3e8470f026ccd8f12b8a67121
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-29 16:09:30 -0800

    Update Ep29_TryCatch Program.cs

--- Ep29_TryCatch/SharpTutorial/Program.cs
+++ Ep29_TryCatch/SharpTutorial/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+
+                string input = Console.ReadLine();
+
+                try
+                {
+                    int number = Convert.ToInt32(input);
+                    Console.WriteLine(number);
+                }
+                catch (Exception ex)
+                {
+                    if(ex is FormatException)
+                        Console.WriteLine("Error! You must enter a number!");
+                    else if(ex is OverflowException)
+                        Console.WriteLine("Error! Your number is too big!");
+                    else
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
