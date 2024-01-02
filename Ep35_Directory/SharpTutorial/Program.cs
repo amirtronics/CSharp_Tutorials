c61fc947bd51155f43507ae38434b1722e23bcf8
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2024-01-02 09:14:21 -0800

    Update Ep35_Directory Program.cs

--- Ep35_Directory/SharpTutorial/Program.cs
+++ Ep35_Directory/SharpTutorial/Program.cs
@@ -7,15 +7,28 @@ namespace SharpTutorial
         static void Main(string[] args)
         {
 
-            string absoluteDirectoryPath = @"C:\Application\Files";
-            string relativeDirectoryPath = @"logs\errorlogs";
+            string absoluteDirectoryPath = @"L:\Application\Files";
+            string relativeDirectoryPath = @"logs\backupLogs";
+            string destRelativeDirectoryPath = @"logs\backupLogs";
 
-            ////create directory
-            //Directory.CreateDirectory(absoluteDirectoryPath);
+            /*
+            string root = Directory.GetDirectoryRoot(absoluteDirectoryPath)
+
+
+            //create directory
+            Directory.CreateDirectory(absoluteDirectoryPath);
             //Directory.CreateDirectory(relativeDirectoryPath);
 
-            bool exists = Directory.Exists(relativeDirectoryPath);
-            string[] files = Directory.GetFiles(relativeDirectoryPath);
+            string[] files;
+
+            if(Directory.Exists(relativeDirectoryPath))
+                files = Directory.GetFiles(relativeDirectoryPath);
+            else //do something else
+            */
+
+            //Directory.Move(relativeDirectoryPath, destRelativeDirectoryPath);
+
+            Directory.Delete(relativeDirectoryPath, true);
 
             Console.WriteLine("Done");
             Console.ReadLine();
