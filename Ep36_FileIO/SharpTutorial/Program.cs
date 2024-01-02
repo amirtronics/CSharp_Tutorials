16d72a61b506eefa1e36459ad7a7b47713f38a29
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2024-01-02 09:33:06 -0800

    Update Ep36_FileIO Program.cs

--- Ep36_FileIO/SharpTutorial/Program.cs
+++ Ep36_FileIO/SharpTutorial/Program.cs
@@ -12,9 +12,44 @@ namespace SharpTutorial
             string fileName = "log.txt";
             string filePath = Path.Combine(path, fileName);
 
-            File.WriteAllText(filePath, "hello world");
+            //File.WriteAllText(filePath, "hello world");
+            //File.WriteAllText(filePath, "A");
 
-            Console.WriteLine(filePath);
+            //File.AppendAllText(filePath, "Log item 1\n");
+            //File.AppendAllText(filePath, "Log item 1\n");
+            //File.AppendAllText(filePath, "Log item 1\n");
+            //File.AppendAllText(filePath, "Log item 1\n");
+
+            bool exists = File.Exists(filePath);
+            if (exists)
+            {
+                string contents = File.ReadAllText(filePath);
+
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+                string copyPath = @"C:\testFiles\copiedFile.txt";
+                if (!File.Exists(filePath))
+                    File.Copy(filePath, copyPath);
+                else
+                {
+                    //promp/warn
+                }
+
+                //string renamePath = @"C:\testFiles\renamedFile.txt";
+                //File.Move(copyPath, renamePath);
+
+                File.Delete(copyPath);
+
+
+                // File.Create();
+            }
+
+
+            //Console.WriteLine(contents);
 
             Console.ReadLine();
         }
