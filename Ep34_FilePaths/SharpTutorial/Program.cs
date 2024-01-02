aa9ca71564ea27522409bf77ba9a0f5a70f5ad2f
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2024-01-01 20:03:03 -0800

    Update Ep34_FilePaths Program.cs

--- Ep34_FilePaths/SharpTutorial/Program.cs
+++ Ep34_FilePaths/SharpTutorial/Program.cs
@@ -6,8 +6,16 @@ namespace SharpTutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //absolute path
+            string testFile1 = @"C:\test\testFile1.txt"; //complete path to a specific location
+
+            //relative paths
+            string testFile2 = @"testFile2.txt"; // same directory as your executable
+            string testFile3 = @"..\testFile3.txt"; // one folder up from your executable
+            string testFile4 = @"testFiles\testFile4.txt"; // inside a directory "testfile" in same directory as your executable
+
             Console.ReadLine();
+
         }
     }
 }
