4cbaa7d3557b5b30ff316fbface15347acc68f72
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2024-01-02 14:40:58 -0800

    Update Ep37_CodingChallenge Doctor.cs

--- Ep37_CodingChallenge/SharpTutorial/Doctor.cs
+++ Ep37_CodingChallenge/SharpTutorial/Doctor.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SharpTutorial
+﻿namespace SharpTutorial
 {
-    internal class Doctor
+    internal class Doctor : Employee, IPageable
     {
+        public string Specialty { get; set; }
+
+        public void PageStaff()
+        {
+
+        }
     }
 }
