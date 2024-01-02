6dbbb26e91db9472df0738156e50ff87d3a51886
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2024-01-01 21:52:12 -0800

    Update Ep35_Directory Program.cs

--- Ep35_Directory/SharpTutorial/Program.cs
+++ Ep35_Directory/SharpTutorial/Program.cs
@@ -6,7 +6,18 @@ namespace SharpTutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string absoluteDirectoryPath = @"C:\Application\Files";
+            string relativeDirectoryPath = @"logs\errorlogsssss";
+
+            ////create directory
+            //Directory.CreateDirectory(absoluteDirectoryPath);
+            //Directory.CreateDirectory(relativeDirectoryPath);
+
+            bool exists = Directory.Exists(relativeDirectoryPath);
+            string[] files = Directory.GetFiles(relativeDirectoryPath);
+
+            Console.WriteLine("Done");
             Console.ReadLine();
         }
     }
