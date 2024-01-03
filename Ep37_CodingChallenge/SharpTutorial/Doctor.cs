abf0570b2633ea9ff0c444277db614cbc4a54e24
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2024-01-03 08:53:43 -0800

    Update Ep37_CodingChallenge Doctor.cs

--- Ep37_CodingChallenge/SharpTutorial/Doctor.cs
+++ Ep37_CodingChallenge/SharpTutorial/Doctor.cs
@@ -2,10 +2,9 @@
 {
     internal class Doctor : Employee, IPageable
     {
-
         public void PageStaff()
         {
-
+            Console.WriteLine("Paging Doctor " + LastName + "!");
         }
     }
 }
